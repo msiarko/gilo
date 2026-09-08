5a511db066a1e6c03d1fc100825d131b5c8e812b
Mikhail Siarko <mikhail.siarko@gmail.com>
2026-09-08 18:38:30 +0200

    Added graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use gilo::{HidDevice, HidDeviceInfo, IntoHidDevice};
-use tokio::task::JoinSet;
+use tokio::{
+    select,
+    signal::ctrl_c,
+    sync::broadcast::{self, Receiver},
+    task::JoinSet,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let (tx, _) = broadcast::channel(1);
     let device_infos = gilo::scan_devices()?.filter(is_logitech_device);
     let mut join_set = JoinSet::new();
     for device_info in device_infos {
         let hid_device_result = device_info.into_hid_device().await;
         match hid_device_result {
             Ok(device) => {
-                join_set.spawn(process_messages(device));
+                let rx = tx.subscribe();
+                join_set.spawn(process_messages(device, rx));
             }
-            Err(err) => eprintln!("{err}"),
+            Err(_) => continue,
         }
     }
 
-    join_set.join_all().await;
+    select! {
+        _ = join_set.join_all() => {},
+        _ = ctrl_c() => {
+            _ = tx.send(());
+        }
+    }
+
     Ok(())
 }
 
@@ -23,8 +36,8 @@ fn is_logitech_device(device_info: &HidDeviceInfo) -> bool {
     device_info.vendor == 0x046d
 }
 
-async fn process_messages(mut device: HidDevice) {
-    loop {
+async fn process_messages(mut device: HidDevice, cancel: Receiver<()>) {
+    while cancel.is_empty() {
         match device.read().await {
             Ok(msg) => {
                 let path = device.path().display();
